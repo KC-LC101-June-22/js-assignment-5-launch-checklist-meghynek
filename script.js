8f800186428de53e73b5faf27be79333384054e5
Megan <meganhynek@gmail.com>
2022-08-18 19:09:11 -0500

    rockin and rollin

--- script.js
+++ script.js
@@ -10,7 +10,13 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       // Below this comment, call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   
+   let document = this.document;0
+   let list = null;
+   let pilotInput = document.querySelector("pilotName");
+
+   formSubmission(document, list, pilotInput, copilot, fuelLevel, cargoLevel){
+
+   }
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,55 +2,66 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
 function validateInput(testInput) {
-    if (testInput === ''){
+    if (testInput === '') {
         return "Empty"
-       } else if (isNaN(testInput) === true){
-         return "Not a Number"
-        } else if (typeof(testInput) === "number"){
-         return "Is a Number"
-        }
+    } else if (isNaN(testInput) === true) {
+        return "Not a Number"
+    } else if (typeof (testInput) === "number") {
+        return "Is a Number"
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   // document?
+    this.document = document
+    // list?
+this.list = list
+    // pilot
+    this.pilot = pilot;
+    if (validateInput(pilot) === "Is a Number") {
+        document.getElementById('pilotStatus').innerHTML = '<li>`Pilot &{pilot} is ready`</li>'
+    } else if (validateInput(pilot) === "Empty") {
+        alert("All fields required.")
+    } else if (validateInput(pilot) === "Is a Number") {
+        alert("Please enter the pilot's name.")
+    }
 
-   // list?
-
-   // pilot
-   this.pilot = pilot;
-   if (validateInput(pilot) === "Is a Number"){
-// rejoice?
-   } else if (validateInput(pilot) === "Empty"){
-    alert("All fields required.")
-   } else if (validateInput(pilot) === "Is a Number"){
-    alert("Please enter the pilot's name.")
-   }
-
-// copilot
-this.copilot = copilot;
-
+    // copilot
+    this.copilot = copilot;
+    if (validateInput(copilot) === "Is a Number") {
+        // rejoice?
+    } else if (validateInput(copilot) === "Empty") {
+        alert("All fields required.")
+    } else if (validateInput(copilot) === "Is a Number") {
+        alert("Please enter the copilot's name.")
+    }
+    // fuelLevel
+    this.fuelLevel = fuelLevel;
+    if (fuelLevel <= 100000) {
+        alert("There is not enough fuel for the journey.")
+    } else if (f)
+//cargoLevel
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -61,5 +72,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
